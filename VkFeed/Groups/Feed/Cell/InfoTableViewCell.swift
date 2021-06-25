9ce7772988210a16b20007800dea14923e5af722
MaksimTsarevich <43068564+MaksimTsarevich@users.noreply.github.com>
2021-06-25 18:22:08 +0300

    Create InfoTableViewCell.swift

---
+++ VkFeed/Groups/Feed/Cell/InfoTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  InfoTableViewCell.swift
+//  VkFeed
+//
+//  Created by Maks Tsarevich on 23.06.21.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+
+    // - UI
+    @IBOutlet weak var countLakes: UILabel!
+    @IBOutlet weak var countComments: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
