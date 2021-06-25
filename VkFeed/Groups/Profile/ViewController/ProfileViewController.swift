87a377bb219b9c499ec0b65b76db8f1afe1442fb
MaksimTsarevich <43068564+MaksimTsarevich@users.noreply.github.com>
2021-06-25 18:21:33 +0300

    Update ProfileViewController.swift

--- VkFeed/Groups/Profile/ViewController/ProfileViewController.swift
+++ VkFeed/Groups/Profile/ViewController/ProfileViewController.swift
@@ -6,24 +6,22 @@
 //
 
 import UIKit
+import VK_ios_sdk
 
 class ProfileViewController: UIViewController {
-
+    
+    // - UI
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    // - Data
+    
+    var user: ProfileModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        userNameLabel.text = user.firstname + " " + user.lastname
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
