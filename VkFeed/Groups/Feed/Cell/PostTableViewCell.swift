62497ecbfdf3c5611f9e1c405331a1ed529464a0
MaksimTsarevich <43068564+MaksimTsarevich@users.noreply.github.com>
2021-06-25 18:22:06 +0300

    Create PostTableViewCell.swift

---
+++ VkFeed/Groups/Feed/Cell/PostTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostTableViewCell.swift
+//  VkFeed
+//
+//  Created by Maks Tsarevich on 23.06.21.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var PostTextLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
